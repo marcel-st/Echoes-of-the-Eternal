f1b66e06e44526e281093939c895f86eb807d9b9
Marcel Stangenberger <github@xo.nl>
2026-04-13 15:12:29 +0000

    Bootstrap Godot 4 RPG starter scaffold

---
+++ core/save/save_data.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name SaveData
+
+@export var save_version: int = 1
+@export var map_scene_path: String = "res://scenes/world/starter_map.tscn"
+@export var spawn_id: String = "start"
+@export var player_position: Vector2 = Vector2(960, 540)
+@export var playtime_seconds: int = 0
+@export var world_flags: Dictionary = {}
+@export var active_quests: Dictionary = {}
+@export var completed_quests: PackedStringArray = []
---
+++ scenes/main.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+@onready var world_root: Node = $WorldRoot
+
+
+func _ready() -> void:
+	if not EventBus.request_ui_prompt.is_connected(_on_ui_prompt):
+		EventBus.request_ui_prompt.connect(_on_ui_prompt)
+
+	SceneRouter.load_initial_map(world_root)
+	EventBus.request_ui_prompt.emit("World loaded. Explore the map. Press E to trigger dialogue.")
+
+
+func _on_ui_prompt(_text: String) -> void:
+	# HUD listens directly to EventBus and renders prompts.
+	pass
